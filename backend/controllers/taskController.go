05b70f3d110daca58fb77011fab336a4bef6c237
Raji, Yusril <yusril.raji@thermofisher.com>
2021-08-26 00:25:20 +0200

    implemented get task by id and update task

--- backend/controllers/taskController.go
+++ backend/controllers/taskController.go
@@ -3,9 +3,8 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
-
-	//"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 
 	"github.com/yusrilmr/todolist/backend/common"
 	"github.com/yusrilmr/todolist/backend/data"
@@ -129,63 +128,69 @@ func GetTaskById(w http.ResponseWriter, r *http.Request) {
 //	w.Write(j)
 //}
 //
-//// UpdateTask update an existing Task document
-//// Handler for HTTP Put - "/tasks/{id}"
-//func UpdateTask(w http.ResponseWriter, r *http.Request) {
-//	// Get id from the incoming url
-//	vars := mux.Vars(r)
-//	id := bson.ObjectIdHex(vars["id"])
-//	var dataResource TaskResource
-//	// Decode the incoming Task json
-//	err := json.NewDecoder(r.Body).Decode(&dataResource)
-//	if err != nil {
-//		common.DisplayAppError(
-//			w,
-//			err,
-//			"Invalid Task data",
-//			500,
-//		)
-//		return
-//	}
-//	task := &dataResource.Data
-//	task.Id = id
-//	context := NewContext()
-//	defer context.Close()
-//	col := context.DbCollection("tasks")
-//	repo := &data.TaskRepository{C: col}
-//	// Update an existing Task document
-//	if err := repo.Update(task); err != nil {
-//		common.DisplayAppError(
-//			w,
-//			err,
-//			"An unexpected error has occurred",
-//			500,
-//		)
-//		return
-//	}
-//	w.WriteHeader(http.StatusNoContent)
-//
-//}
-//
-//// DeleteTask deelete an existing Task document
-//// Handler for HTTP Delete - "/tasks/{id}"
-//func DeleteTask(w http.ResponseWriter, r *http.Request) {
-//	vars := mux.Vars(r)
-//	id := vars["id"]
-//	context := NewContext()
-//	defer context.Close()
-//	col := context.DbCollection("tasks")
-//	repo := &data.TaskRepository{C: col}
-//	// Delete an existing Task document
-//	err := repo.Delete(id)
-//	if err != nil {
-//		common.DisplayAppError(
-//			w,
-//			err,
-//			"An unexpected error has occurred",
-//			500,
-//		)
-//		return
-//	}
-//	w.WriteHeader(http.StatusNoContent)
-//}
+// UpdateTask update an existing Task document
+// Handler for HTTP Put - "/tasks/{id}"
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	// Get id from the incoming url
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var dataResource TaskResource
+	// Decode the incoming Task json
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"Invalid Task data",
+			500,
+		)
+		return
+	}
+	task := &dataResource.Data
+	u64, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}
+	task.ID = uint(u64)
+	context := NewContext().PostgresDB
+	repo := &data.TaskRepository{C: context}
+	// Update an existing Task document
+	if err := repo.Update(task); err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+// DeleteTask deelete an existing Task document
+// Handler for HTTP Delete - "/tasks/{id}"
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	context := NewContext().PostgresDB
+	repo := &data.TaskRepository{C: context}
+	// Delete an existing Task
+	err := repo.Delete(id)
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
--- backend/data/taskRepository.go
+++ backend/data/taskRepository.go
@@ -26,11 +26,12 @@ func (r *TaskRepository) Update(task *models.Task) error {
 	}).Error
 	return err
 }
+
 func (r *TaskRepository) Delete(id string) error {
-	//err := r.C.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
 	err := r.C.Delete(&models.Task{}, id).Error
 	return err
 }
+
 func (r *TaskRepository) GetAll() []models.Task {
 	//var tasks []models.Task
 	//iter := r.C.Find(nil).Iter()
@@ -43,11 +44,12 @@ func (r *TaskRepository) GetAll() []models.Task {
 	//results := r.C.Find(&tasks)
 	return tasks
 }
+
 func (r *TaskRepository) GetById(id string) (task models.Task, err error) {
-	//err = r.C.FindId(bson.ObjectIdHex(id)).One(&task)
 	err = r.C.First(&task, id).Error
 	return
 }
+
 func (r *TaskRepository) GetByUser(user string) []models.Task {
 	//var tasks []models.Task
 	//iter := r.C.Find(bson.M{"createdby": user}).Iter()
